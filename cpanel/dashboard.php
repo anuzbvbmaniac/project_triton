0c64cb2f0ef558222627c7a399de45c4ee8379d4
Anuz Pandey <anuzbvbmaniac123@gmail.com>
2018-03-01 00:46:52 +0545

    So  I changed login page and dashboard with a ad banner page

--- cpanel/dashboard.php
+++ cpanel/dashboard.php
@@ -1,10 +1,195 @@
 <?php 
-session_start();
-if (!isset($_SESSION['user_logged'])){
-	header('Location:index.php?loginfirst=yes');
-}
-include('navigation.php');
-?>
-<div>
-	<!-- your code here -->
-</div>
+   session_start();
+   if (!isset($_SESSION['user_logged'])){
+   	header('Location:index.php?loginfirst=yes');
+   }
+   ?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="Triton School, College">
+      <meta name="keywords" content="Triton">
+      <meta name="author" content="Anuz Pandey">
+      <!-- Title -->
+      <title>Triton - Admin Dashboard</title>
+      <!-- Styles -->
+      <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+      <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="../assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+      <link href="../assets/plugins/icomoon/style.css" rel="stylesheet">
+      <link href="../assets/plugins/uniform/css/default.css" rel="stylesheet"/>
+      <link href="../assets/plugins/switchery/switchery.min.css" rel="stylesheet"/>
+      <link href="../assets/plugins/nvd3/nv.d3.min.css" rel="stylesheet">
+      <!-- Theme Sty -->
+      <link href="../assets/css/triton.css" rel="stylesheet">
+      <link href="../assets/css/themes/admin.css" rel="stylesheet">
+      <link href="../assets/css/custom.css" rel="stylesheet">
+   </head>
+   <body>
+      <!-- Page Container -->
+      <div class="page-container">
+         <!-- Page Sidebar -->
+         <div class="page-sidebar">
+            <a class="logo-box" href="index.html">
+            <span>Triton</span>
+            <i class="fa fa-lock"></i>
+            </a>
+            <div class="page-sidebar-inner">
+               <div class="page-sidebar-menu">
+                  <ul class="accordion-menu">
+                     <li class="active-page">
+                        <a href="dashboard.php">
+                        <i class="menu-icon icon-home4"></i><span>Dashboard</span>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="adBanner.php">
+                        <i class="menu-icon fa fa-image"></i><span>Ad-Setting</span>
+                        </a>
+                     </li>
+                     <li class="menu-divider"></li>
+                     <li>
+                        <a href="#">
+                        <i class="menu-icon icon-help_outline"></i><span>Documentation</span>
+                        </a>
+                     </li>
+                     <li>
+                        <a href="#">
+                        <i class="menu-icon icon-public"></i><span>Changelog</span><span class="label label-danger">1.0</span>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+         <!-- /Page Sidebar -->
+         <!-- Page Content -->
+         <div class="page-content">
+            <!-- Page Header -->
+            <div class="page-header">
+               <!--Search Form-->
+               <div class="search-form">
+                  <form action="#" method="GET">
+                     <div class="input-group">
+                        <input type="text" name="search" class="form-control search-input" placeholder="Type something...">
+                        <span class="input-group-btn">
+                        <button class="btn btn-default" id="close-search" type="button"><i class="icon-close"></i></button>
+                        </span>
+                     </div>
+                  </form>
+               </div>
+               <!--// search form-->
+               <nav class="navbar navbar-default">
+                  <div class="container-fluid">
+                     <!-- Brand and toggle get grouped for better mobile display -->
+                     <div class="navbar-header">
+                        <div class="logo-sm">
+                           <a href="javascript:void(0)" id="sidebar-toggle-button"><i class="fa fa-bars"></i></a>
+                           <a class="logo-box" href="index.html"><span>Triton</span></a>
+                        </div>
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <i class="fa fa-angle-down"></i>
+                        </button>
+                     </div>
+                     <!-- Collect the nav links, forms, and other content for toggling -->
+                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                           <li><a href="javascript:void(0)" id="collapsed-sidebar-toggle-button"><i class="fa fa-bars"></i></a></li>
+                           <li><a href="javascript:void(0)" id="toggle-fullscreen"><i class="fa fa-expand"></i></a></li>
+                           <li><a href="javascript:void(0)" id="search-button"><i class="fa fa-search"></i></a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                           <li><a href="javascript:void(0)" class="right-sidebar-toggle" data-sidebar-id="main-right-sidebar"><i class="fa fa-envelope"></i></a></li>
+                           <li class="dropdown">
+                              <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-bell"></i></a>
+                              <ul class="dropdown-menu dropdown-lg dropdown-content">
+                                 <li class="drop-title">Notifications<a href="#" class="drop-title-link"><i class="fa fa-angle-right"></i></a></li>
+                                 <li class="slimscroll dropdown-notifications">
+                                    <ul class="list-unstyled dropdown-oc">
+
+                                       
+                                    </ul>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li class="dropdown user-dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="../assets/images/admin.png" alt="" class="img-circle"></a>
+                              <ul class="dropdown-menu">
+                                 <li><a href="#">Profile</a></li>
+                                 <li><a href="#"><span class="badge pull-right badge-danger">42</span>Messages</a></li>
+                                 <li role="separator" class="divider"></li>
+                                 <li><a href="#">Account Settings</a></li>
+                                 <li><a href="logout.php">Log Out</a></li>
+                              </ul>
+                           </li>
+                        </ul>
+                     </div>
+                     <!-- /.navbar-collapse -->
+                  </div>
+                  <!-- /.container-fluid -->
+               </nav>
+            </div>
+            <!-- /Page Header -->
+            <!-- Page Inner -->
+            <div class="page-inner">
+               <div class="page-title">
+                  <h3 class="breadcrumb-header">Admin Dashboard</h3>
+               </div>
+               <div id="main-wrapper">
+                  <div class="row">
+                     <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-white stats-widget">
+                           <div class="panel-body">
+                              <div class="pull-left">
+                                 <span class="stats-number">$781,876</span>
+                                 <p class="stats-info">Total Income</p>
+                              </div>
+                              <div class="pull-right">
+                                 <i class="icon-arrow_upward stats-icon"></i>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <!-- Row -->
+                  <div class="row">
+                  </div>
+                  <!-- Row -->
+               </div>
+               <!-- Main Wrapper -->
+               <div class="page-footer">
+                  <style>
+                     .namawebaAnuz:hover{
+                     color: gold;
+                     }
+                  </style>
+                  <p>Made with <i class="fa fa-heart red-color"></i> by <a target="_blank" class="namawebaAnuz" href="https://www.linkedin.com/in/anuj-pandey-832620138/">Anuz Pandey</a></p>
+               </div>
+            </div>
+            <!-- /Page Inner -->
+         </div>
+         <!-- /Page Content -->
+      </div>
+      <!-- /Page Container -->
+      <!-- Javascripts -->
+      <script src="../assets/plugins/jquery/jquery-3.1.0.min.js"></script>
+      <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+      <script src="../assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+      <script src="../assets/plugins/uniform/js/jquery.uniform.standalone.js"></script>
+      <script src="../assets/plugins/switchery/switchery.min.js"></script>
+      <script src="../assets/plugins/d3/d3.min.js"></script>
+      <script src="../assets/plugins/nvd3/nv.d3.min.js"></script>
+      <script src="../assets/plugins/flot/jquery.flot.min.js"></script>
+      <script src="../assets/plugins/flot/jquery.flot.time.min.js"></script>
+      <script src="../assets/plugins/flot/jquery.flot.symbol.min.js"></script>
+      <script src="../assets/plugins/flot/jquery.flot.resize.min.js"></script>
+      <script src="../assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
+      <script src="../assets/plugins/flot/jquery.flot.pie.min.js"></script>
+      <script src="../assets/plugins/chartjs/chart.min.js"></script>
+      <script src="../assets/js/triton.js"></script>
+      <script src="../assets/js/pages/dashboard.js"></script>
+   </body>
+</html>
--- cpanel/index.php
+++ cpanel/index.php
@@ -1,29 +1,30 @@
 <!DOCTYPE html>
 <html>
-<head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Triton School, College">
-        <meta name="keywords" content="Triton">
-        <meta name="author" content="Anuz Pandey">
-        
-        <!-- Title -->
-        <title>Triton - Admin Dashboard</title>
 
-        <!-- Styles -->
-        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-        <link href="../assets/plugins/icomoon/style.css" rel="stylesheet">
-        <link href="../assets/plugins/uniform/css/default.css" rel="stylesheet"/>
-        <link href="../assets/plugins/switchery/switchery.min.css" rel="stylesheet"/>
-        <link href="../assets/plugins/nvd3/nv.d3.min.css" rel="stylesheet">  
-      
-        <!-- Theme Styles -->
-        <link href="../../assets/css/triton.css" rel="stylesheet">
-        <link href="../../assets/css/themes/admin.css" rel="stylesheet">
-        <link href="../../assets/css/custom.css" rel="stylesheet">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Triton School, College">
+	<meta name="keywords" content="Triton">
+	<meta name="author" content="Anuz Pandey">
+
+	<!-- Title -->
+	<title>Triton - Admin Login</title>
+
+	<!-- Styles -->
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+	<link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="../assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../assets/plugins/icomoon/style.css" rel="stylesheet">
+	<link href="../assets/plugins/uniform/css/default.css" rel="stylesheet"/>
+	<link href="../assets/plugins/switchery/switchery.min.css" rel="stylesheet"/>
+	<link href="../assets/plugins/nvd3/nv.d3.min.css" rel="stylesheet">
+
+	<!-- Theme Styles -->
+	<link href="../assets/css/triton.css" rel="stylesheet">
+	<link href="../assets/css/themes/admin.css" rel="stylesheet">
+	<link href="../assets/css/custom.css" rel="stylesheet">
 </head>
 
 <body>
@@ -46,120 +47,131 @@
 
 	}
 	?>
-	<div class="container">
-		<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-			<div class="panel panel-info">
-				<div class="panel-heading">
-					<div class="panel-title">Sign In</div>
-					<!-- <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div> -->
-				</div>
-				<?php
-				//Invalid Username or Password
-				if ( isset( $_GET[ 'warning' ] ) ) {
-					?>
-				<div class="alert alert-dismissible alert-danger">
-					<button type="button" class="close" data-dismiss="alert">&times;</button>
-					<?php
-					echo 'Invalid Username or Password';
-					?>
-				</div>
-				<?php
-				}
-				?>
-				<?php
+	<!-- Page Container -->
+	<div class="page-container">
+		<!-- Page Inner -->
+		<div class="page-inner login-page">
+			<div id="main-wrapper" class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-6 col-md-3 login-box">
+						<img class="img-responsive" src="../assets/images/Triton-International.png" alt="">
+						<h4 class="login-title">Sign in to your account</h4>
+						<form id="loginform" class="form-horizontal" role="form" action="" method="POST">
+							<?php
+							//Invalid Username or Password
+							if ( isset( $_GET[ 'warning' ] ) ) {
+								?>
+							<div class="alert alert-dismissible alert-danger">
+								<button type="button" class="close" data-dismiss="alert">&times;</button>
+								<?php
+								echo 'Invalid Username or Password';
+								?>
+							</div>
+							<?php
+							}
+							?>
+							<?php
 
 
-				//Block Direct Access to dashboard(MSG)
-				if ( isset( $_GET[ 'loginfirst' ] ) ) {
-					?>
-				<div class="alert alert-danger">
-					<?php
-					echo 'Please Login First';
-					?>
-				</div>
-				<?php
-				}
-				?>
-				<div style="padding-top:30px" class="panel-body">
+							//Block Direct Access to dashboard(MSG)
+							if ( isset( $_GET[ 'loginfirst' ] ) ) {
+								?>
+							<div class="alert alert-danger">
+								<?php
+								echo 'Please Login First';
+								?>
+							</div>
+							<?php
+							}
+							?>
 
-					<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-
-					<form id="loginform" class="form-horizontal" role="form" action="" method="POST">
-
-						<div style="margin-bottom: 25px" class="input-group">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-							<input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email" required="required">
-						</div>
-
-						<div style="margin-bottom: 25px" class="input-group">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-							<input id="login-password" type="password" class="form-control" name="password" placeholder="password" required="required">
-						</div>
-
-						<div class="input-group">
-							<div class="checkbox">
+							<div class="form-group">
+								<label for="exampleInputEmail1">Username</label>
+								<input id="login-username" type="text" class="form-control" name="username" value="" placeholder="Username">
+							</div>
+							<div class="form-group">
+								<label for="exampleInputPassword1">Password</label>
+								<input id="login-password" type="password" class="form-control" name="password" placeholder="Password">
+							</div>
+							<div class="form-group">
 								<label>
 									<input id="showPassword" onclick="functionShowPassword()" type="checkbox" name="showPassword" value="1"> Show Password
 								</label>
 							
 							</div>
-						</div>
-						<div style="margin-top:10px" class="form-group">
-							<!-- Button -->
-
-							<div class="col-sm-12 controls">
-								<input type="submit" id="btn-login" class="btn btn-success" name="login" value="Login">
-
+							<div class="form-group">
+								<input type="submit" id="btn-login" class="btn btn-blue" name="login" value="Login">
 							</div>
-						</div>
-					</form>
+							<div class="form-group">
+								<a href="#" class="forgot-link">Forgot password?</a>
+							</div>
+							
+							
+							
+							
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-		<script type="text/javascript">
-			function functionShowPassword() {
-				var x = document.getElementById( "login-password" );
-				if ( x.type === "password" ) {
-					x.type = "text";
-				} else {
-					x.type = "password";
-				}
+		<!-- /Page Content -->
+	</div>
+	<!-- /Page Container -->
+
+
+	<script type="text/javascript">
+		function functionShowPassword() {
+			var x = document.getElementById( "login-password" );
+			if ( x.type === "password" ) {
+				x.type = "text";
+			} else {
+				x.type = "password";
 			}
-		</script>
+		}
+	</script>
+
+	<!-- Javascripts -->
+	<script src="../assets/plugins/jquery/jquery-3.1.0.min.js"></script>
+	<script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<script src="../assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="../assets/plugins/uniform/js/jquery.uniform.standalone.js"></script>
+	<script src="../assets/plugins/switchery/switchery.min.js"></script>
+	<script src="../assets/js/triton.js"></script>
 
 </body>
 
 </html>
-<?php 
-	session_start();
-	require_once('../connection.php');
-	if(isset($_POST['login'])){
-		$username=$_POST['username'];
-		$password=md5($_POST['password']);
-		// echo $password;
-		// die();
-		$query_check = "SELECT * FROM tbl_admin WHERE username = '$username'  AND password ='$password'";
-		$result = $conn->query( $query_check );
-		if ( $result->num_rows > 0 ) {
+<?php
+session_start();
+require_once( '../connection.php' );
+if ( isset( $_POST[ 'login' ] ) ) {
+	$username = $_POST[ 'username' ];
+	$password = md5( $_POST[ 'password' ] );
+	// echo $password;
+	// die();
+	$query_check = "SELECT * FROM tbl_admin WHERE username = '$username'  AND password ='$password'";
+	$result = $conn->query( $query_check );
+	if ( $result->num_rows > 0 ) {
 
-			while ( $data = $result->fetch_assoc() ) {
-				$id = $data[ 'id' ];
-				$user = $data[ 'username' ];
+		while ( $data = $result->fetch_assoc() ) {
+			$id = $data[ 'id' ];
+			$user = $data[ 'username' ];
 
-			}
-
-			$_SESSION['user_logged']=TRUE;
-			$_SESSION["uid"] = $id;
-
-			header('Location:dashboard.php?loginsucc=yes');
-		}else {
-			if ( isset( $_COOKIE[ 'login_count' ] ) ) {
-				$_COOKIE[ 'login_count' ] = $_COOKIE[ 'login_count' ] + 1;
-				setcookie( "login_count", $_COOKIE[ 'login_count' ], time() + 60 );
-			} else {
-				setcookie( "login_count", 1, time() + 60 );
-			}
-			header('Location:index.php?warning=loginfailed');
 		}
+
+		$_SESSION[ 'user_logged' ] = TRUE;
+		$_SESSION[ "uid" ] = $id;
+
+		header( 'Location:dashboard.php?loginsucc=yes' );
+	} else {
+		if ( isset( $_COOKIE[ 'login_count' ] ) ) {
+			$_COOKIE[ 'login_count' ] = $_COOKIE[ 'login_count' ] + 1;
+			setcookie( "login_count", $_COOKIE[ 'login_count' ], time() + 60 );
+		} else {
+			setcookie( "login_count", 1, time() + 60 );
+		}
+		header( 'Location:index.php?warning=loginfailed' );
 	}
-	?>
+}
+?>
