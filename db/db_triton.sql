7f3b864227bcb806070147983a3aa856bee8f5c1
Tsahveen Magar <magartsahveen@gmail.com>
2018-02-28 22:21:18 +0545

    done

--- db/db_triton.sql
+++ db/db_triton.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2018 at 02:01 PM
+-- Generation Time: Feb 28, 2018 at 05:35 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -42,6 +42,24 @@ INSERT INTO `tbl_admin` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_banner`
+--
+
+CREATE TABLE `tbl_banner` (
+  `banner_id` int(11) NOT NULL,
+  `banner_file` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_banner`
+--
+
+INSERT INTO `tbl_banner` (`banner_id`, `banner_file`) VALUES
+(1, '6926c979ee2c8315eb7140dfac8437d1_Trinton.png');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_feedback`
 --
 
@@ -49,6 +67,7 @@ CREATE TABLE `tbl_feedback` (
   `feedback_id` int(11) NOT NULL,
   `feedback_in` int(11) NOT NULL,
   `feedback_by` varchar(100) NOT NULL,
+  `phone` varchar(10) NOT NULL,
   `email` varchar(100) NOT NULL,
   `feedback` text NOT NULL,
   `feedback_date` datetime NOT NULL
@@ -58,11 +77,12 @@ CREATE TABLE `tbl_feedback` (
 -- Dumping data for table `tbl_feedback`
 --
 
-INSERT INTO `tbl_feedback` (`feedback_id`, `feedback_in`, `feedback_by`, `email`, `feedback`, `feedback_date`) VALUES
-(1, 0, 'Olga Stokes', 'pigoli@yahoo.com', 'Veniam aliquid aliquam quia illo facere', '2018-02-27 12:36:50'),
-(5, 0, 'Latifah Francis', 'xakobidi@yahoo.com', 'Adipisicing voluptas molestiae quo est quia autem hic reprehenderit qui corrupti qui sit adipisci ea', '2018-02-27 12:38:30'),
-(6, 0, 'Theodore Spears', 'dyruk@yahoo.com', 'Quas sapiente reprehenderit obcaecati non dolorem reprehenderit eaque rerum atque impedit', '2018-02-27 12:42:25'),
-(9, 0, 'Sabin Thapa', 'lenexebal@gmail.com', 'Vero minus ex magni sunt consequatur Voluptatem accusantium dolor et voluptatem', '2018-02-27 13:06:02');
+INSERT INTO `tbl_feedback` (`feedback_id`, `feedback_in`, `feedback_by`, `phone`, `email`, `feedback`, `feedback_date`) VALUES
+(15, 1, 'Conan Cabrera', '2147483647', 'Email Not Available', 'Omnis quia voluptatem et minima aut pariatur Laborum Culpa adipisicing elit eu animi proident fugiat dolor enim reprehenderit', '2018-02-28 15:15:00'),
+(17, 1, 'Declan Salas', '9876543454', 'senoxyjamo@yahoo.com', 'Ut nostrum elit optio dolor nemo velit consectetur iusto vel ea sed irure laboris', '2018-02-28 15:18:08'),
+(18, 3, 'Michelle George', '9876543454', 'sysamy@gmail.com', 'Modi aut cupidatat consectetur sint omnis saepe quia proident minus non laborum', '2018-02-28 15:35:47'),
+(19, 2, 'Anastasia Silva', '9751234565', 'Email Not Available', 'Quo consequatur et aut explicabo Nihil quo tempor eum aut saepe', '2018-02-28 15:43:07'),
+(20, 4, 'Tara Shields', '9865764332', 'bytisaqyh@hotmail.com', 'Debitis ipsa sunt cillum deserunt rerum tempor illo eligendi ad quia debitis consectetur magni dolor consequatur do facilis est', '2018-02-28 15:45:59');
 
 -- --------------------------------------------------------
 
@@ -75,6 +95,16 @@ CREATE TABLE `tbl_level` (
   `level_name` varchar(100) NOT NULL,
   `updated_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_level`
+--
+
+INSERT INTO `tbl_level` (`level_id`, `level_name`, `updated_date`) VALUES
+(1, 'School', '0000-00-00'),
+(2, 'Plus Two', '0000-00-00'),
+(3, 'Bachelor', '0000-00-00'),
+(4, 'Master', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -122,6 +152,12 @@ ALTER TABLE `tbl_admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_banner`
+--
+ALTER TABLE `tbl_banner`
+  ADD PRIMARY KEY (`banner_id`);
+
+--
 -- Indexes for table `tbl_feedback`
 --
 ALTER TABLE `tbl_feedback`
@@ -155,15 +191,20 @@ ALTER TABLE `tbl_stream`
 ALTER TABLE `tbl_admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT for table `tbl_banner`
+--
+ALTER TABLE `tbl_banner`
+  MODIFY `banner_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `tbl_feedback`
 --
 ALTER TABLE `tbl_feedback`
-  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT for table `tbl_level`
 --
 ALTER TABLE `tbl_level`
-  MODIFY `level_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `level_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `tbl_notice`
 --
